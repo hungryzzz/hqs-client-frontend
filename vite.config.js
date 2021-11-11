bb86c243471dec10b01cc9f06e8fce9c1ebc20fe
Young_f <16307130335@fudan.edu.cn>
2021-11-11 18:22:34 +0800

    first

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css:{
+    preprocessorOptions: {
+      less: {
+        modifyVArs: {
+          'archblue-6': '#4785ac'
+        }
+      }
+    }
+  }
+})
