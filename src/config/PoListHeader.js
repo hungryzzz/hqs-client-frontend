80186e345ba21950e7bbc475b1ee417146425780
fduxuan <285526932@qq.com>
2022-09-18 01:29:08 +0800

    polist table

--- src/config/PoListHeader.js
+++ src/config/PoListHeader.js
@@ -1,33 +1,34 @@
 /*
  * @Created on: 2022-09-16 23:50:04
- * @@LastEditTime: 2022-09-17 00:19:36
+ * @@LastEditTime: 2022-09-18 01:26:17
  * @@Author: ring
  * 
  * @@Desc: 
  * 
  */
+// [header name, width]
 export const PoListHeader = [
-  "STATUS",
-  "PO#",
-  "SORT#",
-  "PART#",
-  "Start Date",
-  "LINE#",
-  "PO Date",
-  "PO Stop Date",
-  "Unit Price",
-  "Part Limit",
-  "Line Amount",
-  "Part Completed",
-  "Part Remaining",
-  "Remaining %",
-  "Billing",
+  ["STATUS", "status", 90, "left"],
+  ["PO#", "po_num", 130, "left"],
+  ["SORT#", "sort_num", 110, "left"],
+  ["PART#", "part_num", 135, ""],
+  ["Start Date", "start_date", 100, ""],
+  ["LINE#", "line_num", 70, "", "line-header-style"],
+  ["PO Date", "po_date", 95, "", "line-header-style"],
+  ["PO Stop Date", "po_stop_date", 125, "", "line-header-style"],
+  ["Unit Price", "unit_price", 100, "", "line-header-style"],
+  ["Part Limit", "part_limit", 100, "", "line-header-style"],
+  ["Line Amount", "line_amount", 140, "", "line-header-style"],
+  ["Part Completed", "part_completed", 135, ""],
+  ["Part Remaining", "part_remaining", 130, ""],
+  ["Remaining %", "remaining", 120, "right"],
+  ["Billing", "billing", 70, "right"],
 ];
 
 export const SpanHeader = [
   "status",
-  "po#",
-  "sort#",
-  "part#",
-  "start date",
+  "po_num",
+  "sort_num",
+  "part_num",
+  "start_date",
 ];
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,47 @@
 <template>
-  <div>
+  <div class="table-wrapper">
     <a-space direction="vertical" size="large" style="width: 100%">
-      <a-table :columns="columns" :data="poList" :span-method="dataSpanMethod" :bordered="{wrapper: true, cell: true}" />
+      <a-table 
+        :columns="columns" 
+        :data="poList" 
+        :span-method="dataSpanMethod" 
+        :bordered="{wrapper: true, cell: true}"
+        column-resizable
+        :pagination="false"
+        :table-layout-fixed="true">
+        <template #status="{ record, rowIndex }">
+          <a-tag color="#f53f3f">{{ record.status }}</a-tag>
+        </template>
+        <template #po_num="{ record, rowIndex }">
+          <span class="text-bold">{{ record.po_num }}</span>
+        </template>
+        <template #sort_num="{ record, rowIndex }">
+          <a-link icon status="success">{{ record.sort_num }}</a-link>
+        </template>
+        <template #part_num="{ record, rowIndex }">
+          <a-space direction="vertical">
+            <a-tag v-for="(p, index) of record.part_num" :key="index">
+              {{ p }}
+            </a-tag>
+          </a-space>
+        </template>
+        <template #remaining="{ record, rowIndx }">
+          <a-tag v-if="parseInt(record.remaining) === 0" color="#00b42a">{{ record.remaining }}%</a-tag>
+          <a-tag v-else color="#ffb400">{{ record.remaining }}%</a-tag>
+        </template>
+        <template #unit_price="{ record, rowIndex }">
+          <span class="text-bold">{{ record.unit_price }}</span>
+        </template>
+        <template #line_amount="{ record, rowIndex }">
+          <span class="text-bold">{{ record.line_amount }}</span>
+        </template>
+      </a-table>
     </a-space>
   </div>
 </template>
 
 <script>
+// 216 129 119
 import { PoListHeader, SpanHeader } from '../config/PoListHeader.js';
 
 export default {
@@ -15,14 +50,23 @@ export default {
   props: {},
   setup() {
     const dataSpanMethod = ({ record, column }) => {
-      if (record["start date"] === record["po date"] && SpanHeader.indexOf(column.dataIndex) !== -1) {
+      if (record["start_date"] === record["po_date"] && SpanHeader.indexOf(column.dataIndex) !== -1) {
         return {
           rowspan: record.line_sum,
           colspan: 1,
         }
       }
     };
-    const columns = PoListHeader.map(co => ({ title: co, dataIndex: co.toLowerCase() }));
+    const columns = PoListHeader.map(co => ({ 
+      title: co[0], 
+      dataIndex: co[1].toLowerCase(), 
+      slotName: co[1].toLowerCase(),
+      width: co[2],
+      fixed: co[3],
+      align: "center",
+      "header-cell-class": co[4],
+    }));
+
     return {
       dataSpanMethod,
       columns,
@@ -34,71 +78,71 @@ export default {
         {
           id: "sdjksjdk",
           status: "CLOSE",
-          "po#": "490032832189",
-          "sort#": "102407",
-          "part#": "15645243-97-B,15645243-97-C",
-          "start date": "10/26/21",
-          "line#": "10",
-          "po date": "10/26/21",
-          "po stop date": "11/26/21",
-          "unit price": "$3.45",
-          "part limit": "2800",
-          "line amount": "$43934.00",
-          "part completed": "2800",
-          "part remaining": "0",
-          "remaining %": "100%",
+          "po_num": "490032832189",
+          "sort_num": "102407",
+          "part_num": ["15645243-97-B", "15645243-97-C"],
+          "start_date": "10/26/21",
+          "line_num": "10",
+          "po_date": "10/26/21",
+          "po_stop_date": "11/26/21",
+          "unit_price": "$3.45",
+          "part_limit": "2800",
+          "line_amount": "$43934.00",
+          "part_completed": "2800",
+          "part_remaining": "0",
+          "remaining": "0.00",
           "line_sum": 4,
         },
         {
           id: "sdj029301jdk",
           status: "CLOSE",
-          "po#": "490032832189",
-          "sort#": "102407",
-          "part#": "15645243-97-B,15645243-97-C",
-          "start date": "10/26/21",
-          "line#": "30",
-          "po date": "11/26/21",
-          "po stop date": "12/26/21",
-          "unit price": "$3.45",
-          "part limit": "232100",
-          "line amount": "$433213934.00",
-          "part completed": "2800",
-          "part remaining": "0",
-          "remaining %": "100%",
+          "po_num": "490032832189",
+          "sort_num": "102407",
+          "part_num": ["15645243-97-B", "15645243-97-C"],
+          "start_date": "10/26/21",
+          "line_num": "30",
+          "po_date": "11/26/21",
+          "po_stop_date": "12/26/21",
+          "unit_price": "$3.45",
+          "part_limit": "232100",
+          "line_amount": "$433213934.00",
+          "part_completed": "2800",
+          "part_remaining": "0",
+          "remaining": "0.00",
         },
         {
           id: "sdjks31mkk",
           status: "CLOSE",
-          "po#": "490032832189",
-          "sort#": "102407",
-          "part#": "15645243-97-B,15645243-97-C",
-          "start date": "10/26/21",
-          "line#": "40",
-          "po date": "12/26/21",
-          "po stop date": "01/26/22",
-          "unit price": "$3.45",
-          "part limit": "98300",
-          "line amount": "$43932134.00",
-          "part completed": "2800",
-          "part remaining": "0",
-          "remaining %": "100%",
+          "po_num": "490032832189",
+          "sort_num": "102407",
+          "part_num": ["15645243-97-B", "15645243-97-C"],
+          "start_date": "10/26/21",
+          "line_num": "40",
+          "po_date": "12/26/21",
+          "po_stop_date": "01/26/22",
+          "unit_price": "$3.45",
+          "part_limit": "98300",
+          "line_amount": "$43932134.00",
+          "part_completed": "2800",
+          "part_remaining": "0",
+          "remaining": "0.00",
         },
         {
           id: "sd39109ljdk",
           status: "CLOSE",
-          "po#": "490032832189",
-          "sort#": "102407",
-          "part#": "15645243-97-B,15645243-97-C",
-          "start date": "10/26/21",
-          "line#": "50",
-          "po date": "01/26/22",
-          "po stop date": "02/26/22",
-          "unit price": "$3.45",
-          "part limit": "2800",
-          "line amount": "$43934.00",
-          "part completed": "2800",
-          "part remaining": "0",
-          "remaining %": "100%",
+          "po_num": "490032832189",
+          "sort_num": "102407",
+          "part_num": ["15645243-97-B", "15645243-97-C"],
+          "start_date": "10/26/21",
+          "line_num": "50",
+          "po_date": "01/26/22",
+          "po_stop_date": "02/26/22",
+          "unit_price": "$3.45",
+          "part_limit": "2800",
+          "line_amount": "$43934.00",
+          "part_completed": "2800",
+          "part_remaining": "0",
+          "remaining": "53.32",
         }
       ],
     }
@@ -109,4 +153,13 @@ export default {
 </script>
 
 <style scoped>
+.table-wrapper {
+  padding: 15px 10px;
+}
+.text-bold {
+  font-weight: 500;
+}
+.line-header-style {
+  background-color: #ff7d00;
+}
 </style>
