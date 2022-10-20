a7e6acbf841851f3530378ce7a12c340c8202396
fduxuan <285526932@qq.com>
2022-10-20 22:49:24 +0800

    普通用户

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Created on: 2022-10-20 22:38:44
+ * @LastEditTime: 2022-10-20 22:43:44
+ * @Author: fduxuan
+ * 
+ * @Desc:  
+ * 
+-->
+<template>
+  <div>
+    <PoListJob v-if="$store.state.user=='Tesla'"></PoListJob>
+    <OrdinaryCustomers v-else></OrdinaryCustomers>
+  </div>
+</template>
+
+<script>
+import OrdinaryCustomers from "./OrdinaryCustomers.vue"
+import PoListJob from "./PoListJob.vue"
+export default {
+  name: "Home",
+  props: {},
+  components: {OrdinaryCustomers, PoListJob},
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+  unmounted() {},
+  watch: {}
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/OrdinaryCustomers.vue
@@ -0,0 +1,56 @@
+<!--
+ * @Created on: 2022-10-17 19:48:12
+ * @LastEditTime: 2022-10-20 22:44:31
+ * @Author: fduxuan
+ * 
+ * @Desc:   普通用户的
+ * 
+-->
+<template>
+  <div>
+    <div class="modal-title">
+        <IconList /> Daily Summary Report
+    </div>
+    <div v-for="item in this.sortList">
+        <SortCard :sortNum="item" id="item"></SortCard>
+    </div>
+    
+
+  </div>
+</template>
+
+<script>
+import SortCard from "../components/OrdinaryCustomers/SortCard.vue"
+import PoDetailService from "../models/PoListService"
+export default {
+  name: "OrdinaryCustomers",
+  props: {},
+  components: {SortCard},
+  data() {
+    return {
+      sortList: []
+    }
+  },
+  methods: {
+    async getSortList(){
+      this.sortList = await PoDetailService.getSortList()
+    }
+  },
+  mounted() {
+      this.getSortList()
+  },
+  unmounted() {},
+  watch: {}
+}
+</script>
+
+<style scoped>
+.modal-title {
+  background-color: rgb(var(--primary-6));
+  color: white;
+  line-height: 38px;
+  font-weight: 600;
+  text-align: left;
+  padding: 0 5%;
+}
+</style>
